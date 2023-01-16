88e87683119b86a4a4380105aa9fd180815ffb48
Joe Davitt <joseph.j.davitt@gmail.com>
2023-01-16 22:08:14 +0000

    create form and bring in API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,88 @@
-import logo from './logo.svg';
+
 import './App.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: [],
+      error: false,
+      errorMessage: ''
+    }
+  }
+
+  getCityData = async (e) => {
+    e.preventDefault();
+
+
+    try {
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`
+
+      let cityDataFromAxios = await axios.get(url);
+      console.log(cityDataFromAxios);
+      this.setState({
+        cityData: cityDataFromAxios.data[0],
+        error: false
+      })
+
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      })
+    }
+
+
+
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      city: e.target.value
+    })
+  }
+
+
+  render() {
+    return (
+      <>
+
+        <h1>City Explorer</h1>
+        <form onSubmit={this.getCityData}>
+          <label htmlFor=''>
+            <input type='text' onInput={this.handleInput} />
+            <button type='submit'>Explore!</button>
+          </label>
+
+        </form>
+
+        {
+          this.state.error
+            ? <p>{this.state.errorMessage}</p>
+            : <Card>
+              <Card.Body>
+                <Card.Title>{this.state.cityData.display_name}</Card.Title>
+                <Card.Text>{this.state.cityData.lat}</Card.Text>
+                <Card.Text>{this.state.cityData.lon}</Card.Text>
+              </Card.Body>
+            </Card>
+
+        }
+
+      </>
+    )
+  }
+
+
+
 }
+
+
 
 export default App;
